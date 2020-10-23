fa8caf5cb948b99e620bbb07c8ca73c1a9dcd86f
Maruri Sanjay Kumar <skrmaruri@dsmartsys.in>
2020-10-23 15:42:01 +0530

    Add project files.

---
+++ VK SpirometerChart/MainWindow.xaml.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MirDataTypes;
+using MirDeviceManager;
+using MirCharting;
+using VideokallSBCDataAcquisionApp.comm;
+using System.Windows.Threading;
+
+namespace VK_SpirometerChart
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Commmodule commtoDAQ = new Commmodule();
+        double appheight = 800;
+        double apptop = 50;
+        public MainWindow()
+        {
+            InitializeComponent();
+            commtoDAQ.ReceivedMessage += ReceivedMessage;
+            commtoDAQ.initialize();
+
+          
+            this.WindowState = WindowState.Maximized;
+            double W = System.Windows.SystemParameters.PrimaryScreenWidth;
+            double H = System.Windows.SystemParameters.PrimaryScreenHeight;
+
+             
+            //FVchart.Height = 300; //300; //300; //700;
+            //FVchart.Width = 390;// 390; //390; //200;// 400;
+             
+            FVchart.Height = 300; //300; //300; //700;
+            FVchart.Width = 390;// 390; //390; //200;// 400;
+            VTchart.Width = 400;
+            VTchart.Height = 200;
+
+            this.Topmost = true;
+            //   this.Hide();
+            // FVchart.ChartColor = Color.re
+            this.WindowState = WindowState.Normal;
+            this.WindowState = WindowState.Minimized;
+        }
+
+        void ReceivedMessage(string msg)
+        {
+            string[] cmd = msg.ToLower().Split('>');
+            
+            switch (cmd[0])
+            {
+                case "<startspirovc":
+                    string[] ht1 = cmd[2].Split(':');
+                    this.Dispatcher.Invoke(DispatcherPriority.Send,
+                   new Action(delegate
+                   {
+                       this.Top = 200;// Convert.ToDouble(ht[1] ) ;
+
+                       this.Left = 0;// Convert.ToDouble(ht[0]);
+                       this.Width = Convert.ToDouble(ht1[3]) + 215;
+                       this.Height = Convert.ToDouble(ht1[2]) - 105;
+                       appheight = Convert.ToDouble(ht1[2]) - 105; ;
+                       apptop = 200;
+                       count = 0;
+                       this.Topmost = Topmost;
+                       if (Height < 750)
+                       {
+                           BtnFullView.IsEnabled = true;
+                       }
+                       else
+                       {
+                           BtnFullView.IsEnabled = false;
+                       }
+                       VTchart.ClearCurveDataPointsFromGraph();
+                       FVchart.ClearCurveDataPointsFromGraph();
+                       this.WindowState = WindowState.Normal;
+                   }));
+
+                 
+                    break;
+                case "<startspirofvc":
+                    string[] ht = cmd[2].Split(':');
+                    this.Dispatcher.Invoke(DispatcherPriority.Send,
+                   new Action(delegate
+                   {
+                       this.Top = 200;// Convert.ToDouble(ht[1] ) ;
+
+                       this.Left = 0;// Convert.ToDouble(ht[0]);
+                       this.Width = Convert.ToDouble(ht[3])+215;
+                      this.Height = Convert.ToDouble(ht[2])-105;
+                       appheight = Convert.ToDouble(ht[2]) - 105; ;
+                       apptop = 200;
+                       if (Height<750)
+                       {
+                           BtnFullView.IsEnabled = true; 
+                       }
+                       else
+                       {
+                           BtnFullView.IsEnabled = false;
+                       }
+                        
+
+                       this.WindowState = WindowState.Normal;
+                       count = 0;
+                       VTchart.ClearCurveDataPointsFromGraph();
+                       FVchart.ClearCurveDataPointsFromGraph();
+                   }));
+
+                   
+                    break;
+                case "spirofvcvt":
+      //  Application.Current.Dispatcher.BeginInvoke(
+      //  DispatcherPriority.Background,
+      //  new Action(() =>
+      //  {
+      //      string[] vdata = cmd[1].Split(',');
+      //      double vol = Convert.ToDouble(vdata[0]);
+      //      double time = Convert.ToDouble(vdata[1]);
+      //      VTchart.AddPointToLine(vol, time);
+      //  }
+      //  ));
+        DisplayVolumeTimeData(cmd[1]);
+                    break;
+                case "spirofvc":
+                    //  DisplayFlowVolumeData(cmd[1]);
+
+            Application.Current.Dispatcher.BeginInvoke(
+            DispatcherPriority.Background,
+            new Action(() => {
+            string[] vdata = cmd[1].Split(',');
+            double flow = Convert.ToDouble(vdata[0]);
+            double vol = Convert.ToDouble(vdata[1]);
+            count++;
+            ///  this.WindowState = WindowState.Minimized;
+          //  TxtLbl.Text = "";
+         //   TxtLbl.Text = count.ToString();
+            FVchart.AddPointToLine(flow, vol);
+        }));
+
+                
+                    break;
+                case "spirovc":
+
+                    DisplayVolumeTimeData(cmd[1]);
+                    break;
+
+                case "<stopspiro":
+                    this.Dispatcher.Invoke(DispatcherPriority.Send,
+                     new Action(delegate
+                         {
+                             this.WindowState = WindowState.Minimized;
+
+                         }));
+                   
+                    break;
+            }
+          //  commtoDAQ.readDataasync();
+        }
+
+        long count = 0;
+        void DisplayVolumeTimeData(string data)
+        {
+            string[] vdata = data.Split(',');
+            double vol = Convert.ToDouble(vdata[0]);
+            double time = Convert.ToDouble(vdata[1]);
+
+
+            //Dispatcher.BeginInvoke(new Action(delegate
+            //{
+            //    VTchart.AddPointToLine(vol, time);
+            //}));
+            this.Dispatcher.Invoke(DispatcherPriority.Send,
+          new Action(delegate
+          {
+                //  TxtLbl.Text = "";
+                // TxtLbl.Text = data;
+                VTchart.AddPointToLine(vol, time);
+          }));
+
+        }
+        void DisplayFlowVolumeData(string data)
+        {
+            string[] vdata = data.Split(',');
+            double flow =   Convert.ToDouble(vdata[0]);
+            double vol =   Convert.ToDouble(vdata[1]);
+            count++;
+            //Dispatcher.BeginInvoke(new Action(delegate
+            //{
+            //    FVchart.AddPointToLine(flow,vol );
+            //}));
+           
+            this.Dispatcher.Invoke(DispatcherPriority.Send,
+        new Action(delegate
+        {
+            if (count > 2000)
+            {
+                // FVchart.ClearCurveDataPointsFromGraph();
+               //  count = 0;
+               
+            }
+            //this.Show();
+            TxtLbl.Text = "";
+            TxtLbl.Text = count.ToString();
+            FVchart.AddPointToLine(flow, vol);
+           
+        }));
+
+        }
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            commtoDAQ.StopBackgroundThread = true;
+        }
+
+        private void Window_Unloaded(object sender, RoutedEventArgs e)
+        {
+            commtoDAQ.StopBackgroundThread = true;
+        }
+
+        private void Window_Closing_1(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            commtoDAQ.StopBackgroundThread = true;
+         //   e.Cancel =true;
+          
+          //  commtoDAQ.backgroundThread.Join();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            commtoDAQ.StopBackgroundThread = true;
+        }
+
+        private void Window_Closing_2(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            commtoDAQ.StopBackgroundThread = true;
+            e.Cancel = false;
+        }
+
+        private void Window_Unloaded_1(object sender, RoutedEventArgs e)
+        {
+            commtoDAQ.StopBackgroundThread = true;
+        }
+
+        private void BtnClear_Click(object sender, RoutedEventArgs e)
+        {
+            FVchart.ClearCurveDataPointsFromGraph();
+        }
+
+        private void BtnDone_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+            VTchart.ClearCurveDataPointsFromGraph();
+            FVchart.ClearCurveDataPointsFromGraph();
+        }
+
+        bool fullview = false;
+        private void BtnFullView_Click(object sender, RoutedEventArgs e)
+        {
+            fullview  = !fullview;
+            BtnFullView.Content = fullview ? "Normal View" : "Full View";
+            if (fullview)
+            {
+               
+                this.Top = 30;// Convert.ToDouble(ht[1] ) ;
+
+               // this.Left = 0;// Convert.ToDouble(ht[0]);
+                              //  this.Width = Convert.ToDouble(ht[3]) + 215;
+                this.Height = 800;// Convert.ToDouble(ht[2]) - 105;
+            }
+            else
+            {
+                this.Top = apptop;// Convert.ToDouble(ht[1] ) ;
+
+                // this.Left = 0;// Convert.ToDouble(ht[0]);
+                //  this.Width = Convert.ToDouble(ht[3]) + 215;
+                this.Height = appheight;// Convert.ToDouble(ht[2]) - 105;
+            }
+        }
+
+        private void BtnClearvc_Click(object sender, RoutedEventArgs e)
+        {
+            VTchart.ClearCurveDataPointsFromGraph();
+        }
+    }
+}
---
+++ VK SpirometerChart/comm/commmodulewithDAQ.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace VideokallSBCDataAcquisionApp.comm
+{
+   public class Commmodule 
+    {
+        UdpClient udpClient;
+        public int Portnumber { get; set; } = 9889;
+        //   public int SMCPortnumber { get; set; } = 9856;
+        public IPEndPoint SMCEndpoint;
+        public delegate void NotifyMessage(string msg);
+        public delegate void StartTimer( bool b );
+        public NotifyMessage ReceivedMessage;
+      public  Thread backgroundThread = null;
+        public bool StopBackgroundThread { get; set; } = false;
+
+      public  void initialize()
+        {
+           
+            udpClient = new UdpClient(Portnumber);
+            //   IPAddress ipaddress = IPAddress.Parse("192.168.0.17");
+            //  SMCEndpoint = new IPEndPoint(ipaddress, SMCPortnumber);
+            IPEndPoint endpoint = new IPEndPoint(IPAddress.Any, 0);
+            ThreadStart TS = new ThreadStart(readData);
+            backgroundThread = new Thread(TS);
+            backgroundThread.Start();
+         //   readDataasync();
+        }
+
+        void LogMessage(string msg)
+        {
+            try
+            {
+                using (System.IO.StreamWriter file =
+           new System.IO.StreamWriter("exceptionlogs.txt", true))
+                {
+                    file.WriteLine(DateTime.Now.ToString() + ": " + msg);
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+        }
+
+        public async void readDataasync()
+        {
+            try
+            {
+                UdpReceiveResult res = await udpClient.ReceiveAsync();
+                SMCEndpoint = res.RemoteEndPoint;
+                byte[] buffer = res.Buffer;
+
+                string str = Encoding.Unicode.GetString(buffer);
+
+                ReceivedMessage?.Invoke(str);
+                
+                LogMessage(str);
+
+            }
+            catch (Exception ex) { LogMessage(ex.Message); }
+        }
+        public   void readData()
+        {
+
+            try
+            {
+                while (true)
+                {
+                    if (StopBackgroundThread)
+                        break;
+
+                    Console.WriteLine("Waiting for broadcast");
+                    byte[] buffer = udpClient.Receive(ref SMCEndpoint);
+                    string str = Encoding.Unicode.GetString(buffer);
+                    //  string str = Encoding.ASCII.GetString(buffer);
+                    ReceivedMessage?.Invoke(str);
+                  //  Thread.Sleep(10);
+                    
+                    //Console.WriteLine($"Received broadcast from {groupEP} :");
+                   // Console.WriteLine($" {Encoding.ASCII.GetString(bytes, 0, bytes.Length)}");
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                udpClient.Close();
+            }
+
+            //try {
+            //    UdpReceiveResult res = await udpClient.ReceiveAsync();
+            //    SMCEndpoint = res.RemoteEndPoint;
+            //    byte[] buffer = res.Buffer;
+
+            //    string str = Encoding.ASCII.GetString(buffer);
+
+            //    ReceivedMessage?.Invoke(str);
+
+            //    LogMessage(str);
+
+            //} catch (Exception ex) { LogMessage(ex.Message); } 
+        }
+
+        public void SendData(string msg)
+        {
+            try {
+
+                if (SMCEndpoint != null)
+                {
+                    byte[] buffer = Encoding.Unicode.GetBytes(msg);
+                    udpClient.Send(buffer, buffer.Length, SMCEndpoint);
+                }
+
+
+            } catch (Exception ex )
+            {
+                LogMessage(ex.Message);
+            }
+           
+        }
+
+
+
+        public void SendByteData(byte[] buffer)
+        {
+            if (SMCEndpoint != null)
+            {
+              //  byte[] buffer = Encoding.Unicode.GetBytes(msg);
+                udpClient.Send(buffer, buffer.Length, SMCEndpoint);
+            }
+        }
+
+
+        public void Reset()
+        {
+            udpClient.Close();
+            udpClient.Dispose();
+
+        }
+
+    }
+}
